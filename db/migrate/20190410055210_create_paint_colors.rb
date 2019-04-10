7497c8819781ccec918b76e29bdca9a7a7d39a78
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-10 06:24:10 +0000

    Added columns to paint colors table

--- db/migrate/20190410055210_create_paint_colors.rb
+++ db/migrate/20190410055210_create_paint_colors.rb
@@ -1,8 +1,10 @@
 class CreatePaintColors < ActiveRecord::Migration
   def change
     create_table :paint_colors do |t|
-
-      t.timestamps null: false
+      t.string :color_name
+      t.string :brand
+      t.string :color_number
+      t.integer :room_id
     end
   end
 end
