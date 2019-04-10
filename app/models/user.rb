3e315779b62a8f828396f0280a105e30001e8bc0
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-10 06:05:44 +0000

    Modified user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :rooms
+  has_many :paint_colors, through: :rooms
+  has_secure_password
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
