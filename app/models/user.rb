3ed2637260554f1e9154ed6e4757455bc977297c
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-16 05:49:37 +0000

    added logout option once user is logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, :password, presence: true
-  validates :username, uniqueness: true
+  validates_uniqueness_of :username, :message=>"That username has already been taken, please use another."
 end
